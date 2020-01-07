326e60a1916ce813e81729ec3639659de46b8bf1
Pushen Wang <wangpush@amazon.com>
2020-01-07 14:31:00 -0800

    Update aws-c-common

--- cmake/aws-c-common-config.cmake
+++ cmake/aws-c-common-config.cmake
@@ -1,9 +1,10 @@
 set(THREADS_PREFER_PTHREAD_FLAG ON)
-find_package(Threads REQUIRED)
+if (WIN32 OR  UNIX OR MACOS)
+    find_package(Threads REQUIRED)
+endif()
 
 if (BUILD_SHARED_LIBS)
     include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
 else()
     include(${CMAKE_CURRENT_LIST_DIR}/static/@PROJECT_NAME@-targets.cmake)
 endif()
-
